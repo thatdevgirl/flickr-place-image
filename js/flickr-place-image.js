4df3ba6bcef05c6feedd9fec8704c7567776d34f
Joni Halabi <joni@jhalabi.com>
2015-09-21 17:44:52 -0400

    Code commit to actually make this placeholder script work.  And write some instructions too.

--- js/flickr-place-image.js
+++ js/flickr-place-image.js
@@ -1,27 +1,66 @@
 var placeImage = {
-	key:    'a73f8a3031523cf6f71ebc87e9906a74',
-	secret: '6c183f21bd769762',
-
-	requestBase:   'https://api.flickr.com/services/rest/?',
-	requestFormat: 'json',
-
-	searchMethod: 'flickr.photos.search',
+	key:     '1bed842637720a719b5ac90bcca6133c',
+	baseUrl: 'https://api.flickr.com/services/rest/?',
+	format:  'json',
+	method:  'flickr.photos.search',
 
 	getPhotos: function() {
-		var url = this.requestBase + "method=" + this.searchMethod + "&format=" + this.requestFormat + "&api_key=" + this.key + "&tags=cats";
+		var _this = this;
 
-		$.ajax({
-			url: url,
-			success: function(data) {
-				console.log('Success!');
-				console.log(data);
-			}
-		})
-		
-		return url;
+		$('img.placeImage').each(function() {
+			// Get the tag(s) for this placeholder image.
+			var tags = $(this).data('tags');
+
+			// Construct the AJAX call's URL.
+			var url  = _this.getApiUrl(tags);
+
+			// Save the DOM object requiring the placeholder image.
+			_this.img = this;
+
+			// Make the AJAX call.
+			// -- TODO: Required to be async for multiple calls; look into async solutions.
+			$.ajax({
+				url: url,
+				async: false,
+				error: function(xhr, status, err) {
+					console.log('Error! \n' + status + '\n' + err);
+				}
+			});
+		});
+	},
+
+	getApiUrl: function(tags) {
+		return this.baseUrl 
+				+ 'method=' + this.method 
+				+ '&api_key=' + this.key 
+				+ '&format=' + this.format
+				+ '&tags=' + tags;
+	},
+
+	displayPhoto: function(photo) {
+		var src = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg'
+		$(this.img).attr('src', src);
 	}
 }
 
+
+/*
+ * jsonFlickrApi()
+ *
+ * Parameters:
+ *    response = AJAX response from API call.
+ * 
+ * Description:
+ *    Calls the displayPhoto() function from the placeImage namespace; sends 1 random photo
+ *    object from the AJAX response as a parameter.
+ */
+
+function jsonFlickrApi(response) {
+	var rand = Math.floor(Math.random() * 100);	// 100 is the default number of replies.
+	var photo = response.photos.photo[rand];
+
+	placeImage.displayPhoto(photo);
+}
+
+
 var photos = placeImage.getPhotos();
-console.log('Photos:');
-console.log(photos);
